992d65cb36e84388c4c272e35c3866f05df9f07c
mayank0398 <mayank0398@gmail.com>
2019-01-13 20:39:31 +0530

    Changing compiler plugin

--- src/main/java/com/bunkguru/maven/Application.java
+++ src/main/java/com/bunkguru/maven/Application.java
@@ -1,13 +1,28 @@
 package com.bunkguru.maven;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public Application(){
         System.out.println("We re inside the application");
+    }
+
+    public void greet(){
+        List<String> greets = new ArrayList<>();
+
+        greets.add("hellor");
+
+        for(String g : greets){
+            System.out.println(g);
+        }
     }
 
     public static void main(String a[]){
 
         System.out.println("Starting application");
         Application ap = new Application();
+
+        ap.greet();
     }
 }
